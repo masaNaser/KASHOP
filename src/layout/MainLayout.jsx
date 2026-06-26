a622c877c7f681e0b2236ce87886d52039b0b9a3
Masanaser <masaNaser@users.noreply.github.com>
2026-06-26 22:11:01 +0300

    updates

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom"
-import { Footer } from "../components/footer"
+import { Footer } from "../components/Footer"
 import Navbar from "../components/Navbar"
 
 
